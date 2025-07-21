b51c2b9d554e2fd4978b30621f23658af1b9fafd
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-07-22 01:20:19 +0430

    feat: add deleteUser controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -157,3 +157,30 @@ export const updateUser = async (req, res, next) => {
     if (client) client.release();
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  let client;
+  try {
+    const { id } = req.params;
+    client = await pool.connect();
+
+    const result = await client.query(
+      "DELETE FROM users WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User Not Found!" });
+    }
+
+    if (Number(id) !== req.user.id && req.user.role !== "admin") {
+      return res.status(403).json({ error: "Forbidden!" });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    next(err);
+  } finally {
+    if (client) client.release();
+  }
+};
