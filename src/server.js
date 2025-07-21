55fcfef886e053e9e0622880903bbf47cc8b50d5
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-07-21 19:47:33 +0430

    feat: add server.js to set up Express server and test database connection

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import pool from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Test DB Connection
+(async () => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    console.log("✅ DB Connected Successfully!");
+    console.log("Current Time:", result.rows[0].now);
+    client.release();
+  } catch (err) {
+    console.error("❌ DB Connection Failed:", err.message);
+    process.exit(1); // Stop the app if DB fails
+  }
+})();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
+
+export default app;
