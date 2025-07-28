4b02a21f95766e27852b64acea267b38854dbfbe
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-07-28 12:28:26 +0430

    feat: add jwt authentication for socket.io

--- src/socket/socketHandler.js
+++ src/socket/socketHandler.js
@@ -1,4 +1,22 @@
+import jwt from "jsonwebtoken";
+
 export const socketHandler = (io) => {
+  io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+
+    if (!token) {
+      return next(new Error("Authentication Error: Token Not Provided!"));
+    }
+
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      socket.user = decode;
+      next();
+    } catch (err) {
+      return next(new Error("Authentication Error: Invalid Token!"));
+    }
+  });
+
   io.on("connection", (socket) => {
     console.log("✅ A User Connected!", socket.id);
 
