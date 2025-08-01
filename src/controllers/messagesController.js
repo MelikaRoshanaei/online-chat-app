0def0c057a09f1df08e02c7e12d19c4fdf0b466a
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-08-02 00:01:45 +0430

    feat: add real-time message deletion via socket.io

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -113,6 +113,20 @@ export const deleteMessage = async (req, res, next) => {
       [id]
     );
 
+    // Real-time delivery via socket.io
+    const { sender_id, receiver_id } = result.rows[0];
+
+    const senderSocketId = onlineUsers.get(sender_id);
+    const receiverSocketId = onlineUsers.get(receiver_id);
+
+    if (senderSocketId) {
+      req.io.to(senderSocketId).emit("messageDeleted", { id });
+    }
+
+    if (receiverSocketId) {
+      req.io.to(receiverSocketId).emit("messageDeleted", { id });
+    }
+
     res.status(200).json(result.rows[0]);
   } catch (err) {
     next(err);
