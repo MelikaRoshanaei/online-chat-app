cfbb31c747e46901fc453b48a50f420dd2cd549b
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-08-11 01:26:35 +0430

    feat: add form state management to SignUp component

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -2,21 +2,49 @@ import Form from "./form";
 import Label from "./label";
 import Input from "./input";
 import Button from "./button";
+import { useState } from "react";
 
 function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO : integrate axios
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <div>
         <Label htmlFor="user-name">Name:</Label>
-        <Input type="text" id="user-name" name="name" />
+        <Input
+          type="text"
+          id="user-name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
         <Label htmlFor="email">Email:</Label>
-        <Input type="email" id="email" name="email" />
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <Label htmlFor="password">Password:</Label>
-        <Input type="password" id="password" name="password" />
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className="flex gap-1">
         <Button className="w-1/2" type="reset">
