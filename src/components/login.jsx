67936238619886b409afa5aeab639508e930e023
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-08-11 01:26:58 +0430

    feat: add form state management to Login component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,17 +2,38 @@ import Form from "./form";
 import Label from "./label";
 import Input from "./input";
 import Button from "./button";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO : integrate axios
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <div>
         <Label htmlFor="login-email">Email:</Label>
-        <Input type="email" id="login-email" name="email" />
+        <Input
+          type="email"
+          id="login-email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <Label htmlFor="login-password">Password:</Label>
-        <Input type="password" id="login-password" name="password" />
+        <Input
+          type="password"
+          id="login-password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className="flex gap-1">
         <Button className="w-1/2" type="reset">
