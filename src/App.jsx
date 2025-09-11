c5eb5c7e5376ef274a51dfb11c37537876ad8296
Melika Roshanaei <melikaroshanaei2003@gmail.com>
2025-09-12 01:48:02 +0330

    feat: implement routing and protected route for /chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import ChatPage from "./components/chatPage.jsx";
+import Login from "./components/login.jsx";
+import SignUp from "./components/signUp.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/chat"
+        element={
+          <ProtectedRoute>
+            <ChatPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
