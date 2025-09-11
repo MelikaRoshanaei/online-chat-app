a55b58ba40da17209934dbfffc64469c6295ce36
Melika Roshanaei <melikaroshanaei2003@gmail.com>
2025-09-12 02:30:38 +0330

    feat: integrate axios in Login component and update styles

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,8 +3,14 @@ import Label from "./label";
 import Input from "./input";
 import Button from "./button";
 import { useState } from "react";
+import { useAuth } from "../context/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -53,56 +59,75 @@ function Login() {
     return newErrors;
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const validationErrors = handleValidation();
     setErrors(validationErrors);
 
     if (Object.keys(validationErrors).length === 0) {
-      // TODO : integrate axios
-      console.log("Form submitted successfully:", formData);
-      handleReset();
+      try {
+        await login(formData.email, formData.password);
+        navigate("/chat");
+      } catch (err) {
+        alert(err.message);
+      }
     }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <div>
-        <Label htmlFor="login-email">Email:</Label>
-        <Input
-          type="email"
-          id="login-email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          variant={errors.email ? "error" : "primary"}
-        />
-        {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-      </div>
-      <div>
-        <Label htmlFor="login-password">Password:</Label>
-        <Input
-          type="password"
-          id="login-password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          variant={errors.password ? "error" : "primary"}
-        />
-        {errors.password && (
-          <p className="text-red-500 text-sm">{errors.password}</p>
-        )}
-      </div>
-      <div className="flex gap-1">
-        <Button className="w-1/2" type="button" onClick={handleReset}>
-          Reset
-        </Button>
-        <Button className="w-1/2" type="submit">
-          Login
-        </Button>
-      </div>
-    </Form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4 py-8">
+      <Form onSubmit={handleSubmit}>
+        <div>
+          <Label htmlFor="login-email">Email:</Label>
+          <Input
+            type="email"
+            id="login-email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            variant={errors.email ? "error" : "primary"}
+          />
+          {errors.email ? (
+            <p className="text-red-500 text-sm mt-1 -mb-1">{errors.email}</p>
+          ) : (
+            <div className="h-3"></div>
+          )}
+        </div>
+        <div>
+          <Label htmlFor="login-password">Password:</Label>
+          <Input
+            type="password"
+            id="login-password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            variant={errors.password ? "error" : "primary"}
+          />
+          {errors.password ? (
+            <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+          ) : (
+            <div className="h-3"></div>
+          )}
+        </div>
+        <div className="flex gap-1">
+          <Button className="w-1/2" type="button" onClick={handleReset}>
+            Reset
+          </Button>
+          <Button className="w-1/2" type="submit">
+            Login
+          </Button>
+        </div>
+        <div className="text-center mt-4 text-sm">
+          <p>
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-blue-500 hover:underline">
+              Sign up here
+            </Link>
+          </p>
+        </div>
+      </Form>
+    </div>
   );
 }
 
