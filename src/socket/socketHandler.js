0a24b8db67123a2c805c28b3f2e2cc022146cd77
Melika Roshanaei <melikaroshanaei2003@gmail.com>
2025-09-13 02:59:50 +0330

    feat: enable httpOnly coockie authentication  for socket.io

--- src/socket/socketHandler.js
+++ src/socket/socketHandler.js
@@ -1,21 +1,25 @@
 import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 export const onlineUsers = new Map();
 
 export const socketHandler = (io) => {
   io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-
-    if (!token) {
-      return next(new Error("Authentication Error: Token Not Provided!"));
-    }
-
     try {
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      socket.user = decodedToken;
-      next();
+      cookieParser()(socket.request, socket.request.res || {}, (err) => {
+        if (err) return next(err);
+
+        const { accessToken } = socket.request.cookies || {};
+        if (!accessToken) {
+          return next(new Error("Authentication Error: No Access Token!"));
+        }
+
+        const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET);
+        socket.user = decodedToken;
+        next();
+      });
     } catch (err) {
-      return next(new Error("Authentication Error: Invalid Token!"));
+      return next(new Error("Authentication Error: ", err.message));
     }
   });
 
